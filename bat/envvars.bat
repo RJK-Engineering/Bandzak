1425c29ebaa9d6eb627bc4f1b1d936f9c28ae042
RJK-Engineering <rjklinkhamer@gmail.com>
2021-07-15 21:05:55 +0200

    Create envvars.bat

---
+++ bat/envvars.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo COMMANDER_DRIVE=%COMMANDER_DRIVE%
+echo COMMANDER_PATH= %COMMANDER_PATH%
+
+echo SystemDrive=    %SystemDrive%
+echo SystemRoot=     %SystemRoot%
+echo ProgramData=    %ProgramData%
+
+echo HOMEDRIVE=      %HOMEDRIVE%
+echo ALLUSERSPROFILE=%ALLUSERSPROFILE%
+echo PUBLIC=         %PUBLIC%
+echo USERPROFILE=    %USERPROFILE%
+echo APPDATA=        %APPDATA%
+echo LOCALAPPDATA=   %LOCALAPPDATA%
+echo TEMP=           %TEMP%
